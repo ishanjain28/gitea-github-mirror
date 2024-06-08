2fbf490d751b667118ee683efc13d10af96c765f
Ishan Jain <contact@ishanjain.me>
2024-06-08 14:02:30 +0530

    added project

--- go.mod
+++ go.mod
@@ -1,3 +1,20 @@
 module gitea-github-mirror
 
 go 1.22.3
+
+require (
+	code.gitea.io/sdk/gitea v0.18.0
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/sethvargo/go-envconfig v1.0.3
+	github.com/sirupsen/logrus v1.9.3
+	golang.org/x/oauth2 v0.21.0
+)
+
+require (
+	github.com/davidmz/go-pageant v1.0.2 // indirect
+	github.com/go-fed/httpsig v1.1.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/go-version v1.6.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,144 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+
+	"code.gitea.io/sdk/gitea"
+	"github.com/google/go-github/github"
+	"github.com/sethvargo/go-envconfig"
+	"golang.org/x/oauth2"
+)
+
+const (
+	DefaultGiteaUrl = "https://git.ishanjain.me"
+)
+
+// Sync every 1 hour 01 minutes
+const DefaultMirrorInterval = "1h01m0s"
+
+type Config struct {
+	GithubUser  string `env:"GITHUB_USER,required"`
+	GithubToken string `env:"GITHUB_TOKEN,required"`
+	GiteaToken  string `env:"GITEA_TOKEN,required"`
+	GiteaUser   string `env:"GITEA_USER,required"`
+	GiteaUrl    string `env:"GITEA_URL"`
+}
+
+func readConfig() Config {
+	ctx := context.Background()
+	config := Config{}
+
+	err := envconfig.Process(ctx, &config)
+	if err != nil {
+		log.Panicf("error in reading env var: %v", err.Error())
+	}
+
+	if config.GiteaUrl == "" {
+		config.GiteaUrl = DefaultGiteaUrl
+	}
+	return config
+}
 
 func main() {
-	fmt.Println("vim-go")
+	config := readConfig()
+
+	// Github API
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: config.GithubToken})
+	oclient := oauth2.NewClient(ctx, ts)
+	ghClient := github.NewClient(oclient)
+
+	giteaClient, err := gitea.NewClient(config.GiteaUrl, gitea.SetBasicAuth(config.GiteaUser, config.GiteaToken))
+	if err != nil {
+		log.Errorf("error in creating client: %v", err.Error())
+	}
+
+	log.Infoln("fetching a list of repositories on Gitea")
+	giteaRepos := listGiteaRepositories(config, giteaClient)
+
+	log.WithFields(log.Fields{
+		"gitea_repo_count": len(giteaRepos),
+	}).Infoln("fetched a list of repositories on gitea")
+
+	// For each repo, Create a a repository on Github with the same visiblity settings
+	// Setup synchronisation operation with the github repo
+
+	for _, repo := range giteaRepos {
+		createRepo(config, repo, ghClient)
+
+		setupMirror(config, repo, giteaClient)
+	}
+}
+
+func setupMirror(config Config, repo *gitea.Repository, client *gitea.Client) {
+
+	client.PushMirrors(repo.Owner.UserName, repo.Name, gitea.CreatePushMirrorOption{
+		Interval:       DefaultMirrorInterval,
+		RemoteAddress:  fmt.Sprintf("https://github.com/%s/%s", config.GithubUser, repo.Name),
+		RemotePassword: config.GithubToken,
+		RemoteUsername: config.GithubUser,
+		SyncONCommit:   true,
+	})
+
+}
+
+func createRepo(config Config, repo *gitea.Repository, client *github.Client) {
+	log.WithFields(log.Fields{
+		"name":    repo.Name,
+		"org":     repo.Owner.UserName,
+		"private": repo.Private,
+	}).Infoln("configuring mirror")
+
+	if repo.OriginalURL != "" {
+		return
+	}
+
+	_, _, err := client.Repositories.Create(context.Background(), "", &github.Repository{
+		Name:        &repo.Name,
+		Private:     &repo.Private,
+		Description: &repo.Description,
+		HasWiki:     &repo.HasWiki,
+		HasProjects: &repo.HasProjects,
+	})
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"name":    repo.Name,
+			"org":     repo.Owner.UserName,
+			"private": repo.Private,
+			"error":   err.Error(),
+		}).Errorln("error in creating repository")
+	}
+
+}
+
+func listGiteaRepositories(config Config, client *gitea.Client) []*gitea.Repository {
+	page := 1
+	output := []*gitea.Repository{}
+
+	for {
+
+		repos, _, err := client.ListUserRepos(config.GiteaUser, gitea.ListReposOptions{
+			ListOptions: gitea.ListOptions{
+				Page:     page,
+				PageSize: 20,
+			},
+		})
+		if err != nil {
+			log.Errorf("error in listing repositories: %v", err.Error())
+			break
+		}
+
+		if len(repos) == 0 {
+			break
+		}
+
+		output = append(output, repos...)
+		page += 1
+	}
+
+	return output
 }
